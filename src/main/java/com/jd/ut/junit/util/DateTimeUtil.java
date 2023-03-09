f765c699351eb31cd67a85e6b204be484ed0fcd3
baoshengmin <baoshengmin@fengbangleasing.com>
2023-03-09 15:56:22 +0800

    commit github

--- src/main/java/com/jd/ut/junit/util/DateTimeUtil.java
+++ src/main/java/com/jd/ut/junit/util/DateTimeUtil.java
@@ -7,6 +7,8 @@ import org.springframework.util.StringUtils;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class DateTimeUtil {
@@ -26,5 +28,42 @@ public class DateTimeUtil {
     public static String dateToString(Date date, String format) {
         return new SimpleDateFormat(format).format(date);
     }
+
+    private class TreeNode{
+        private int val;
+        private TreeNode left;
+        private TreeNode right;
+    }
+
+    private List<TreeNode> list;
+
+    public TreeNode iteraterTreeNode(TreeNode node){
+        if(node == null) return null;
+        if(null != node.right){
+            iteraterTreeNode(node.right);
+        }
+        return node;
+    }
+
+    private LinkedList<TreeNode> list2 = new LinkedList<>();
+    public TreeNode iteraterTreeNode2(TreeNode node){
+        if(node == null) return null;
+        list2.add(node);
+        if(null != node.right){
+            iteraterTreeNode(node.right);
+        } else {
+            list2.remove();
+            iteraterTreeNode(list2.getLast().left);
+        }
+        return node;
+
+    }
+
+    public static void main(String[] args) {
+        String s = "1234";
+        System.out.println(s.charAt(3));
+    }
+
+
 
 }
